0cc5a3a06de7efe8d0f1ed4380bcdffd82039dd8
Jeremy Encalada <encaladajeremy@gmail.com>
2023-07-18 20:24:21 -0500

    Solucion problema CORS

--- prisma/index.js
+++ prisma/index.js
@@ -12,9 +12,12 @@ import tutoriaRoutes from './routes/Tutoria.routes.js'
 const app = express()
 
 app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    next();
-  });
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
+
+
 
 app.use(express.json())
 
